8c8af608174f8d3923976d5a27b5f8b6c2761db7
wangcch <ci713@qq.com>
2020-06-01 19:13:28 +0800

    test(useEventPortal): sync type

--- tests/use-event.test.tsx
+++ tests/use-event.test.tsx
@@ -11,7 +11,7 @@ const contentNode: JSX.Element = <span>{"name"}</span>;
 describe("useEventPortal", () => {
   it("default", () => {
     const { result } = renderHook(() => useEventPortal({}));
-    const [, , onShow, onClose] = result.current;
+    const { onShow, onClose } = result.current;
 
     const el = document.body.querySelector(
       `div[${CONTAINER_ATTR_NAME}="${CONTAINER_ATTR_VALUE}"]`
@@ -19,23 +19,23 @@ describe("useEventPortal", () => {
 
     expect(el).not.toBeNull();
 
-    expect(result.current[1]).toEqual(false);
+    expect(result.current.visiable).toEqual(false);
 
-    const Portal1 = result.current[0];
+    const Portal1 = result.current.Portal;
     expect(mount(<Portal1>{contentNode}</Portal1>).html()).toBeNull();
-    expect(result.current[4].innerHTML).toEqual("");
+    expect(result.current.element.innerHTML).toEqual("");
 
     act(() => {
       onShow();
     });
 
-    expect(result.current[1]).toEqual(true);
+    expect(result.current.visiable).toEqual(true);
 
-    const Portal2 = result.current[0];
+    const Portal2 = result.current.Portal;
     expect(mount(<Portal2>{contentNode}</Portal2>).html()).toEqual(
       renderToStaticMarkup(contentNode)
     );
-    expect(result.current[4].innerHTML).toEqual(
+    expect(result.current.element.innerHTML).toEqual(
       renderToStaticMarkup(contentNode)
     );
 
@@ -43,9 +43,9 @@ describe("useEventPortal", () => {
       onClose();
     });
 
-    expect(result.current[1]).toEqual(false);
+    expect(result.current.visiable).toEqual(false);
 
-    const Portal3 = result.current[0];
+    const Portal3 = result.current.Portal;
     expect(mount(<Portal3>{contentNode}</Portal3>).html()).toBeNull();
   });
 
@@ -54,29 +54,29 @@ describe("useEventPortal", () => {
       useEventPortal({ defaultVisiable: true })
     );
 
-    expect(result.current[1]).toEqual(true);
+    expect(result.current.visiable).toEqual(true);
 
-    const Portal1 = result.current[0];
+    const Portal1 = result.current.Portal;
     expect(mount(<Portal1>{contentNode}</Portal1>).html()).toEqual(
       renderToStaticMarkup(contentNode)
     );
 
     act(() => {
-      result.current[3]();
+      result.current.onClose();
     });
 
-    expect(result.current[1]).toEqual(false);
+    expect(result.current.visiable).toEqual(false);
 
-    const Portal2 = result.current[0];
+    const Portal2 = result.current.Portal;
     expect(mount(<Portal2>{contentNode}</Portal2>).html()).toBeNull();
 
     act(() => {
-      result.current[2]();
+      result.current.onShow();
     });
 
-    expect(result.current[1]).toEqual(true);
+    expect(result.current.visiable).toEqual(true);
 
-    const Portal3 = result.current[0];
+    const Portal3 = result.current.Portal;
     expect(mount(<Portal3>{contentNode}</Portal3>).html()).toEqual(
       renderToStaticMarkup(contentNode)
     );
