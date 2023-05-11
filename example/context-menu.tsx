74576bf1d7c5032d399aeda7440f88c09ae91684
wangcch <wangcch.cc@gmail.com>
2023-05-11 14:25:25 +0800

    docs(context-menu): mask demo

--- example/context-menu.tsx
+++ example/context-menu.tsx
@@ -5,6 +5,7 @@ import * as styles from "./context-menu.styles";
 
 const ContextMenuDemo = () => {
   const [Node, actions] = useContextMenu();
+  const [Node2, actions2] = useContextMenu();
 
   return (
     <div>
@@ -38,6 +39,49 @@ const ContextMenuDemo = () => {
         >
           right click
         </Node.Trigger>
+        Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
+      </div>
+
+      <hr />
+
+      <Node2.Wrapper>
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0,0,0,0.1)",
+          }}
+          onClick={() => actions2.close()}
+          onContextMenu={(e) => {
+            e.preventDefault();
+            actions2.close();
+          }}
+        >
+          mask
+        </div>
+        <div style={{ border: "1px solid #d9d9d9", background: "white" }}>
+          <ul className={styles.styleUl} onClick={() => actions2.close()}>
+            <li>option01</li>
+            <li>option02</li>
+            <li>option03</li>
+          </ul>
+        </div>
+      </Node2.Wrapper>
+
+      <div style={{ textAlign: "left" }}>
+        Paragraph{" "}
+        <Node2.Trigger
+          style={{
+            background: "#36cfc9",
+            display: "inline",
+            marginRight: "8px",
+          }}
+        >
+          right click (with mask)
+        </Node2.Trigger>
         Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph Paragraph
       </div>
     </div>
